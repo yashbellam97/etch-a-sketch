3e60a928fd6b7cfa3f6fec9bceb59e6ed32dd23a
yashbellam97 <yashbellam97@gmail.com>
2020-06-16 20:32:35 -0400

    Change to a random color instead of black

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
 let container = document.querySelector(".container");
 
+let gridSize = document.querySelector("#grid-size");
+
 let clearButton = document.querySelector("#clear-button");
 clearButton.addEventListener("click", clear);
 
-generateGrid(16);
+let initialSize = 16;
+
+generateGrid(initialSize);
 
 function changeColor(event) {
-    event.target.style.backgroundColor = "black";
+    let randomR = Math.random() * 100 + 100;
+    let randomG = Math.random() * 100 + 100;
+    let randomB = Math.random() * 100 + 100;
+    event.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
 }
 
 function clear() {
     container.innerHTML = "";
 
-    let number = 0;
     do {
         number = parseInt(prompt("How many squares per side? (upto 64)"));
     } while (number < 1 || number > 64)
-    
+
+    if(isNaN(number) || number === null) {
+        alert("Invalid number");
+        number = initialSize
+    }
+
     generateGrid(number);
 }
 
 function generateGrid(number) {
+    gridSize.textContent = `Grid size: ${number} x ${number}`;
     container.style.cssText = `grid-template-rows: repeat(${number}, 1fr); grid-template-columns: repeat(${number}, 1fr);`;
 
     for (let i = 0; i < number * number; i++) {
